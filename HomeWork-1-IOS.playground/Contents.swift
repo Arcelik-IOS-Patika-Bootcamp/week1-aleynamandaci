6cee37f0fa6126dfca7b6d161c2cb467a271306f
aleynamandaci <aleynamandaci@gmail.com>
2022-05-19 17:25:02 +0300

    Add new func
    Added new function to calculate grade average

--- HomeWork-1-IOS.playground/Contents.swift
+++ HomeWork-1-IOS.playground/Contents.swift
@@ -23,9 +23,24 @@ class Student {
     init (_ nameSurname : String){
         self.nameSurname = nameSurname
     }
+    
+    // func for calculate grade average
+    func gradeAverage (lectureName : lectureName , midterm : Int?, final : Int?) -> Float {
+            var result = 0
+        
+            if let midtermValue = midterm {
+                result += midtermValue*30
+            }
+            if let finalValue = final {
+                result += finalValue*70
+            }
+            return Float(result)/100
+        }
 }
 
 var student1 = Student("Aleyna Mandacı")
+var student2 = Student("Ali Yılmaz")
+var student3 = Student("Mehmet Er")
 
 
 
